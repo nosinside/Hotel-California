9c4aad6a6a281dda2e486b7fa427efcd77fdd4cd
Darwin Vera Ringele <ringelex@gmail.com>
2016-06-07 22:42:25 -0300

    modificacion del cliente

--- Hotel-California/Hotel California/src/Java/Cliente.java
+++ Hotel-California/Hotel California/src/Java/Cliente.java
@@ -9,5 +9,25 @@
  * @author Johan Barahona
  */
 public class Cliente {
+    private String nombre;
+    private String rut;
+    
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
     
 }
